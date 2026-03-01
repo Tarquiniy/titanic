606fc2285ba83fbfbc9e8e5cd6b511129fd805eb
Tarquiniy <airserfer@rambler.ru>
2026-03-01 14:46:15 +0500

    c

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ import 'package:titanic/theme/app_theme.dart';
 import 'screens/login_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Supabase.initialize(
-    url: 'https://lfgfrqpxmjwmklbrugbp.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmZ2ZycXB4bWp3bWtsYnJ1Z2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNTU4MTEsImV4cCI6MjA4MjkzMTgxMX0.MBCsfcg8h47OTmBGVc4c8iT6kAC8unA2x8Q9PbVO_vA',
-  );
+WidgetsFlutterBinding.ensureInitialized();
+//
+//  await Supabase.initialize(
+//    url: 'https://lfgfrqpxmjwmklbrugbp.supabase.co',
+//    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmZ2ZycXB4bWp3bWtsYnJ1Z2JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNTU4MTEsImV4cCI6MjA4MjkzMTgxMX0.MBCsfcg8h47OTmBGVc4c8iT6kAC8unA2x8Q9PbVO_vA',
+//  );
 
 
 await Supabase.initialize(
